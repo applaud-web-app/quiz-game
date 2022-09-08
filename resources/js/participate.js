34d2b09e99e2ff83ecce75a86ee3271aad25df63
applaud-web-app <101401063+applaud-web-app@users.noreply.github.com>
2022-09-08 16:56:26 +0530

    Update participate.js

--- resources/js/participate.js
+++ resources/js/participate.js
@@ -7,7 +7,6 @@ const channel  = Echo.channel('participants-channel');
 channel.subscribed(()=>{}).listen('.participants',(event)=>{
     console.log(event);
  if(event.com_id==userId || event.user_id==userId){
-    // window.location.href = '/play-game';
     console.log("redirect success");
  }
 });
